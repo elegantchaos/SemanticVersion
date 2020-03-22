f690e457b2406251e72db9c173582ed9cdbd4d64
Sam Deane <sam@elegantchaos.com>
2020-03-22 17:38:11 +0000

    Added string coercion.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# SemanticVersion
+# CommandShell
 
-A description of this package.
+Command line tool support.
+
+Version 2.0 is based on Apple's new swift-argument-parser library.
+
+This package just contains a few utilities providing common / standard functionality for any command line tool.
+
+Version 1.0 was a bespoke system with a completely different API. Commands using this version will require porting to use swift-argument-parser instead.
+
--- Sources/SemanticVersion/SemanticVersion.swift
+++ Sources/SemanticVersion/SemanticVersion.swift
@@ -3,14 +3,23 @@
 //  All code (c) 2020. - present day, Elegant Chaos Limited.
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-public struct SemanticVersion {
-    let major: Int
-    let minor: Int
-    let patch: Int
-    
-    init(_ major: Int, _ minor: Int = 0, _ patch: Int = 0) {
+public struct SemanticVersion: Codable {
+    public let major: Int
+    public let minor: Int
+    public let patch: Int
+
+    public var asString: String {
+        var string = "\(major).\(minor)"
+        if patch != 0 {
+            string += ".\(patch)"
+        }
+        return string
+    }
+
+    public init(_ major: Int, _ minor: Int = 0, _ patch: Int = 0) {
         self.major = major
         self.minor = minor
         self.patch = patch
     }
+    
 }
