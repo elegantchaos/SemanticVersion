b1a1f00299e14a45119f43f2df7058b2939bfde2
Sam Deane <sam@elegantchaos.com>
2020-03-22 17:55:01 +0000

    Added some string conversions.

--- Sources/SemanticVersion/SemanticVersion.swift
+++ Sources/SemanticVersion/SemanticVersion.swift
@@ -17,9 +17,50 @@ public struct SemanticVersion: Codable {
     }
 
     public init(_ major: Int, _ minor: Int = 0, _ patch: Int = 0) {
+        self.init(major: major, minor: minor, patch: patch)
+    }
+    
+    public init(major: Int, minor: Int, patch: Int) {
         self.major = major
         self.minor = minor
         self.patch = patch
     }
-    
+
+    public init?(major: String, minor: String, patch: String) {
+        guard let iMajor = Int(major), let iMinor = Int(minor), let iPatch = Int(patch) else {
+            return nil
+        }
+
+        self.major = iMajor
+        self.minor = iMinor
+        self.patch = iPatch
+    }
+
+    public init?(_ string: String) {
+        var version = string
+        if string.first == "v" {
+            version.remove(at: version.startIndex)
+        }
+        let items = version.split(separator: ".")
+        guard items.count == 3 else {
+            return nil
+        }
+        
+        self.init(major: String(items[0]), minor: String(items[1]), patch: String(items[2]))
+    }
+}
+
+func <(x: SemanticVersion, y: SemanticVersion) -> Bool {
+    if (x.major < y.major) {
+        return true
+    } else if (x.major > y.major) {
+        return false
+    } else if (x.minor < y.minor) {
+        return true
+    } else if (x.minor > y.minor) {
+        return false
+    } else {
+        return x.patch < y.patch
+    }
+
 }
